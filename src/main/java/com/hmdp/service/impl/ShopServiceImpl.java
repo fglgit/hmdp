0557e70b4022de1788fe468bf35317197792415e
Lifeng <gf11222024@163.com>
2025-03-15 16:52:00 +0800

    实现：shop的缓存设置空值”“

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -16,8 +16,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -44,11 +43,16 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop= JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        if(shopJson!=null){
+            //查到了空值说明不存在店铺信息
+            return Result.fail("店铺不存在");
+        }
         //4、不存在，直接mybatis-plus查找
         Shop shop =getById(id);
-        //5、查找不到，则报错
+        //5、查找不到，则报错设置空值
         if(shop == null){
-            return Result.fail("商铺不存在");
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return Result.fail("店铺不存在");
         }
         //6、往redis里面存储
         shopJson=JSONUtil.toJsonStr(shop);
